11f58a8b933d55b728648f3e940594e9ef9dc252
NinaPochernyaeva <nina3333@list.ru>
2020-01-18 20:02:59 +0300

    почти ничего

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ char **ft_map_create(int side)  //функция5, создает карту
     int j;
 
     i = 0;
-    map = (char **)malloc((side) * sizeof(char));
+    map = (char **)malloc((side) * sizeof(char *));
     while (i < side)
     {
         j = 0;
@@ -30,11 +30,22 @@ void ft_print_map(char **map, int side)  //печатает карту
 
     i = 0;
     while (i < side)
-    {
-        ft_putstr(map[i]);
-        ft_putchar('\n');
-        i++;
-    }
+        ft_putendl(map[i++]);
+}
+
+char **ft_fill(char **map, int x, int y, char value)  //заполняет ячейку с заданными координатами заданным символом
+{
+
+    map[y][x] = value;
+
+    /*    int a;
+
+    a = 0;
+    while (map[0][a]) //что, если карта нулевая?
+        a++;  //считаем сторону карты без символа \0
+    **(map + (a + 1) * y + x) = value;
+    */
+    return (map);
 }
 
 int main()
@@ -45,6 +56,7 @@ int main()
     side = 10;
    // side = ft_square_root(n * 4); //n - число тетрамино, хз откуда брать
     map = ft_map_create(side);
+    map = ft_fill(map, 2, 3, 'A');
     ft_print_map(map, side);
     return (0);
 }
